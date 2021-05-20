2c472b6419f76b9c1a3271e770851e5a5cfdfef4
terminal-guy <vishaladhithya19@gmail.com>
2021-05-20 08:01:16 +0530

    adding some notes

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -17,3 +17,9 @@ class CategoriesScreen extends StatelessWidget {
 
 
 
+// * Notes
+// Slivers are just scrollable area on the screen.
+// Grid Delegate takes care of the grid structing.
+// the mainAxisSpacing and crossAxisSpacing get the some spacing between items
+// the maxCrossAxisExtent gives the max cross axis length of 200
+// the childAspectRation 3 / 2 gives the 300 / 200
