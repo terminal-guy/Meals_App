4a54df1115edd8994743b221ed68ebb89f280410
terminal-guy <vishaladhithya19@gmail.com>
2021-05-20 08:45:44 +0530

    mapped Dummy_categories

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
+import './models/category.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
-      ),
-      children: [],
-    );
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  catData.title,
+                  catData.color,
+                ))
+            .toList());
+    // Mapping DUMMY_CATEGORIES to the children of CategoriesScreen
   }
 }
-
-
 
 // * Notes
 // Slivers are just scrollable area on the screen.
