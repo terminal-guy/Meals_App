fc20de6a768e80edf37ab418e71d0d73311c8fa6
terminal-guy <vishaladhithya19@gmail.com>
2021-05-20 19:58:05 +0530

    adding some notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
-y
+import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/category_meals_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
+    );
+  }
+}
+
+// routes are used in order to navigate and pass data through the screens.
+// / notation is used cuz the web dev use it so while navigation through screens
