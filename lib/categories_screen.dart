fbd730a280019a8c768fcb8c16df5be729489482
terminal-guy <vishaladhithya19@gmail.com>
2021-05-20 08:54:51 +0530

    adding a scaffold and a appbar

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,19 +6,22 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(
-                  catData.title,
-                  catData.color,
-                ))
-            .toList());
+    return Scaffold(
+      appBar: AppBar(title: const Text('MealsApp')),
+      body: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(
+                    catData.title,
+                    catData.color,
+                  ))
+              .toList()),
+    );
     // Mapping DUMMY_CATEGORIES to the children of CategoriesScreen
   }
 }
