827f65ee16565b1aa073163edef8debeae36e552
terminal-guy <vishaladhithya19@gmail.com>
2021-05-21 17:13:35 +0530

    Add TabsScreen as the default screen and removed categoriesscreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       //  home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.screenName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.screenname: (ctx) => MealDetailScreen(),
       },
