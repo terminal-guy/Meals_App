c590542b99bce3312b340ba6803e315f3ae91ee4
terminal-guy <vishaladhithya19@gmail.com>
2021-05-21 18:19:33 +0530

    adding some default filters

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const screenName = '/filters';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
