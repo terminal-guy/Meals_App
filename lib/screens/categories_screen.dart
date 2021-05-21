59b43529bc185cc5b9a359f33276737865858234
terminal-guy <vishaladhithya19@gmail.com>
2021-05-21 17:14:26 +0530

    Removing scaffold from the file cuz it can't have a scaffold due to tabbar scaffold is controlled by itself

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,23 +6,21 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('MealsApp')),
-      body: GridView(
-          padding: const EdgeInsets.all(25),
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 20,
-          ),
-          children: DUMMY_CATEGORIES
-              .map((catData) => CategoryItem(
-                    catData.id,
-                    catData.title,
-                    catData.color,
-                  ))
-              .toList()),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ))
+          .toList(),
     );
     // Mapping DUMMY_CATEGORIES to the children of CategoriesScreen
   }
