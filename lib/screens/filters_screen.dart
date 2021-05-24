a4f9d5cbb17444369817203ef8a9db6593c4b169
terminal-guy <vishaladhithya19@gmail.com>
2021-05-24 08:01:55 +0530

    made a simple filterscreen with a scaffold

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const screenName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: MainDrawer(),
+      appBar: AppBar(
+        title: Text('You Filters'),
+      ),
+      body: Center(
+        child: Text('Your Filters'),
+      ),
+    );
   }
 }
